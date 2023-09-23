ad618c43e7252c337b261caf94156d9eac9918e2
thlamz <thlamenza@hotmail.com>
2023-09-23 19:36:13 -0300

    Making communication API more clear

--- simulator/node/communication.py
+++ simulator/node/communication.py
@@ -47,37 +47,42 @@ def can_transmit(source_position: Position, destination_position: Position, comm
 
 
 class CommunicationHandler(INodeHandler):
-    sources: Dict[int, CommunicationSource]
-    destinations: Dict[int, CommunicationDestination]
-    event_loop: EventLoop
+    _sources: Dict[int, CommunicationSource]
+    _destinations: Dict[int, CommunicationDestination]
+    _event_loop: EventLoop
 
     def __init__(self, communication_medium: Medium):
         self._injected = False
 
-        self.sources = {}
-        self.destinations = {}
+        self._sources = {}
+        self._destinations = {}
         self.communication_medium = communication_medium
 
     def inject(self, event_loop: EventLoop):
         self._injected = True
-        self.event_loop = event_loop
+        self._event_loop = event_loop
 
     def register_node(self, node: Node):
         if not self._injected:
             raise CommunicationException("Error registering node: Cannot register node on uninitialized "
                                          "node handler")
-        self.sources[node.id] = CommunicationSource(node)
-        self.destinations[node.id] = CommunicationDestination(node)
+        self._sources[node.id] = CommunicationSource(node)
+        self._destinations[node.id] = CommunicationDestination(node)
 
     def handle_command(self, command:  CommunicationCommand, sender: Node):
+        """
+        Performs a communication command
+        :param command: Command being issued
+        :param sender: Node issuing the command
+        """
         if sender.id == command.destination:
             raise CommunicationException("Error transmitting message: message destination is equal to sender. Try "
                                          "using schedule_timer.")
 
-        source = self.sources[sender.id]
+        source = self._sources[sender.id]
 
         if command.type == CommunicationCommandType.BROADCAST:
-            for destination, endpoint in self.destinations.items():
+            for destination, endpoint in self._destinations.items():
                 if destination != sender.id:
                     self._transmit_message(command.message, source, endpoint)
         else:
@@ -85,17 +90,17 @@ class CommunicationHandler(INodeHandler):
             if destination is None:
                 raise CommunicationException("Error transmitting message: a destination is "
                                              "required when command type SEND is used.")
-            if destination not in self.destinations:
+            if destination not in self._destinations:
                 raise CommunicationException(f"Error transmitting message: destination {destination} does not exist.")
 
-            self._transmit_message(command.message, source, self.destinations[destination])
+            self._transmit_message(command.message, source, self._destinations[destination])
 
     def _transmit_message(self, message: str, source: CommunicationSource, destination: CommunicationDestination):
         if can_transmit(source.node.position, destination.node.position, self.communication_medium):
             if self.communication_medium.delay <= 0:
                 destination.receive_message(message, source)
             else:
-                self.event_loop.schedule_event(
-                    self.event_loop.current_time + self.communication_medium.delay,
+                self._event_loop.schedule_event(
+                    self._event_loop.current_time + self.communication_medium.delay,
                     lambda: destination.receive_message(message, source)
                 )
