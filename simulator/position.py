d8eea490e587f9a29be41bd96703aaff85a9c822
thlamz <thlamenza@hotmail.com>
2023-10-07 12:45:07 -0300

    Implementing squared distance helper for positions

--- simulator/position.py
+++ simulator/position.py
@@ -1,3 +1,8 @@
 from typing import Tuple
 
 Position = Tuple[float, float, float]
+
+
+def squared_distance(start: Position, end: Position) -> float:
+    return (end[0] - start[0]) ** 2 + (end[1] - start[1]) ** 2 + (end[2] - start[2]) ** 2
+
