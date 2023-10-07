31540321503492a444b706a9385ac0303621a713
thlamz <thlamenza@hotmail.com>
2023-10-07 18:19:01 -0300

    Using mobility visualization in ping simulation

--- showcases/ping-pong/main.py
+++ showcases/ping-pong/main.py
@@ -1,5 +1,5 @@
 from simulator.node.handler.communication import CommunicationHandler
-from simulator.node.handler.mobility import MobilityHandler
+from simulator.node.handler.mobility import MobilityHandler, MobilityConfiguration
 from simulator.node.handler.timer import TimerHandler
 from simulator.simulation import SimulationBuilder, SimulationConfiguration
 from ping import PingProtocol
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     builder = SimulationBuilder(SimulationConfiguration(duration=10, debug=True))
     builder.add_handler(CommunicationHandler())
     builder.add_handler(TimerHandler())
-    builder.add_handler(MobilityHandler())
+    builder.add_handler(MobilityHandler(MobilityConfiguration(visualization=True)))
 
     builder.add_node(PingProtocol, (0, 0, 0))
     builder.add_node(PingProtocol, (1, 1, 0))
