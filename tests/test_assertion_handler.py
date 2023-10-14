72c8bff468583e10c5be9e16ffb2c44fc35025bf
thlamz <thlamenza@hotmail.com>
2023-10-14 16:12:59 -0300

    Creating tests for assertion handler

---
+++ tests/test_assertion_handler.py
@@ -0,0 +1,93 @@
+import unittest
+from typing import List
+
+from simulator.messages.telemetry import Telemetry
+from simulator.node.handler.assertion import AssertionHandler, assert_always_true_for_simulation, \
+    FailedAssertionException, assert_always_true_for_protocol
+from simulator.node.handler.mobility import MobilityHandler
+from simulator.node.node import Node
+from simulator.protocols.interface import IProtocol
+from simulator.simulation import SimulationBuilder, SimulationConfiguration
+
+
+class TestAssertionHandler(unittest.TestCase):
+    def test_always_assertion_succesful(self):
+        builder = SimulationBuilder(SimulationConfiguration(max_iterations=2))
+
+        @assert_always_true_for_simulation("always true")
+        def assert_number_of_nodes(nodes: List[Node]):
+            return len(nodes) == 0
+
+        builder.add_handler(AssertionHandler([assert_number_of_nodes]))
+        builder.add_handler(MobilityHandler())
+        builder.build().start_simulation()
+        self.assertTrue(True)
+
+    def test_always_assertion_failure(self):
+        builder = SimulationBuilder(SimulationConfiguration(max_iterations=2))
+
+        @assert_always_true_for_simulation("always false")
+        def assert_number_of_nodes(nodes: List[Node]):
+            return len(nodes) == 0
+
+        class DummyProtocol(IProtocol):
+
+            def initialize(self, stage: int):
+                pass
+
+            def handle_timer(self, timer: str):
+                pass
+
+            def handle_packet(self, message: str):
+                pass
+
+            def handle_telemetry(self, telemetry: Telemetry):
+                pass
+
+            def finish(self):
+                pass
+
+        builder.add_handler(AssertionHandler([assert_number_of_nodes]))
+        builder.add_handler(MobilityHandler())
+        builder.add_node(DummyProtocol, (0, 0, 0))
+        with self.assertRaises(FailedAssertionException):
+            builder.build().start_simulation()
+
+    def verify_protocol_always_assertion(self, success: bool):
+        builder = SimulationBuilder(SimulationConfiguration(max_iterations=1))
+
+        class DummyProtocol(IProtocol):
+            variable = True
+
+            def initialize(self, stage: int):
+                pass
+
+            def handle_timer(self, timer: str):
+                pass
+
+            def handle_packet(self, message: str):
+                pass
+
+            def handle_telemetry(self, telemetry: Telemetry):
+                pass
+
+            def finish(self):
+                pass
+
+        @assert_always_true_for_protocol(DummyProtocol, "always true")
+        def assert_number_of_nodes(node: Node[DummyProtocol]):
+            return node.protocol_encapsulator.protocol.variable \
+                if success else not node.protocol_encapsulator.protocol.variable
+
+        builder.add_handler(AssertionHandler([assert_number_of_nodes]))
+        builder.add_handler(MobilityHandler())
+        builder.add_node(DummyProtocol, (0, 0, 0))
+        builder.build().start_simulation()
+        self.assertTrue(True)
+
+    def test_protocol_always_assertion_success(self):
+        self.verify_protocol_always_assertion(True)
+
+    def test_protocol_always_assertion_failure(self):
+        with self.assertRaises(FailedAssertionException):
+            self.verify_protocol_always_assertion(False)
