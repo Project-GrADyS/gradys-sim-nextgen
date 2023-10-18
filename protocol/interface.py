7cc8caf6290db42517d1abb5da2dd2874989fcdd
thlamz <thlamenza@hotmail.com>
2023-10-18 00:10:04 -0300

    Improving documentation on interfaces

--- protocol/interface.py
+++ protocol/interface.py
@@ -6,29 +6,59 @@ from simulator.provider.interface import IProvider
 
 class IProtocol(ABC):
     provider: IProvider
+    """
+    IProvider instance that will provide protocol
+    with necessary tools to function
+    """
 
     @classmethod
     def instantiate(cls, provider: IProvider) -> 'IProtocol':
+        """
+        Called when the protocol is instantiated before the simulation starts.
+        This function is important because it is where the IProvider instance
+        is injected into the protocol.
+        """
         protocol = cls()
         protocol.provider = provider
         return protocol
 
     @abstractmethod
-    def initialize(self, stage: int):
+    def initialize(self, stage: int) -> None:
+        """
+        This is the first function called when the simulation begins. The initialize()
+        methods for each network node are called in arbitrary order so don't rely on other
+        protocols having already been initialized.
+        """
         pass
 
     @abstractmethod
-    def handle_timer(self, timer: str):
+    def handle_timer(self, timer: str) -> None:
+        """
+        Called when a previously scheduled timer fires
+        :param timer: the timer that fired
+        """
         pass
 
     @abstractmethod
-    def handle_packet(self, message: str):
+    def handle_packet(self, message: str) -> None:
+        """
+        Called when a packet is received from another node.
+        """
         pass
 
     @abstractmethod
-    def handle_telemetry(self, telemetry: Telemetry):
+    def handle_telemetry(self, telemetry: Telemetry) -> None:
+        """
+        Regularly called by the mobility module with information
+        about the state of the node's mobility.
+        """
         pass
 
     @abstractmethod
-    def finish(self):
+    def finish(self) -> None:
+        """
+        Called when the simulation finishes. The finish() method
+        of every node in the simulation is called in arbitrary
+        order.
+        """
         pass
--- simulator/provider/interface.py
+++ simulator/provider/interface.py
@@ -8,18 +8,30 @@ from simulator.messages.mobility import MobilityCommand
 class IProvider(ABC):
     @abstractmethod
     def send_communication_command(self, command: CommunicationCommand):
+        """
+        Sends a communication command to the node's communication module
+        """
         pass
 
     @abstractmethod
     def send_mobility_command(self, command: MobilityCommand):
+        """
+        Sends a mobility command to the node's mobility module
+        """
         pass
 
     @abstractmethod
     def schedule_timer(self, timer: str, timestamp: float):
+        """
+        Schedules a timer that should fire at a specified timestamp
+        """
         pass
 
     @abstractmethod
     def current_time(self) -> float:
+        """
+        Returns the current simulation time
+        """
         pass
 
     tracked_variables: Dict[str, Any]
