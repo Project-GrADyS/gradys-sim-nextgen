f577af6488432adb1e26b1a6d712d6d977d936b4
Thlamz <thlamenza@hotmail.com>
2023-10-24 21:07:59 +0000

    Correcting unnecessarry f-string

--- simulator/node/handler/timer.py
+++ simulator/node/handler/timer.py
@@ -43,7 +43,7 @@ class TimerHandler(INodeHandler):
             raise TimerException(f"Could not set timer: Node {node.id} not registered")
 
         if timestamp < self._event_loop.current_time:
-            raise TimerException(f"Could not set timer: Timer cannot be set in the past")
+            raise TimerException("Could not set timer: Timer cannot be set in the past")
 
         self._event_loop.schedule_event(timestamp,
                                         lambda: node.protocol_encapsulator.handle_timer(message),
