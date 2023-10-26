f3e4a99cd0c4a88102a8aa9bd374ecd0c41c7db6
thlamz <thlamenza@hotmail.com>
2023-10-26 20:34:05 -0300

    Documenting node module

---
+++ docs/Modules/Simulator/node.md
@@ -0,0 +1,3 @@
+# Node
+
+:::simulator.node
--- simulator/node.py
+++ simulator/node.py
@@ -1,3 +1,7 @@
+"""
+This is a simple module containing declarations necessary to keep track of the nodes during the simulation
+"""
+
 from typing import Generic, TypeVar
 
 from protocol.interface import IProtocol
@@ -9,9 +13,14 @@ T = TypeVar("T", bound=IProtocol)
 
 class Node(Generic[T]):
     """
-    Represents a node inside the python simulation,
+    Represents a node inside the python simulation. Holds the reference to the node's encapsulated protocol.
+    This class is accessible to [handlers][simulator.handler].
     """
     id: int
+    """Node's unique identifier"""
+
     protocol_encapsulator: IEncapsulator[T]
+    """Node's encapsulated protocol"""
 
     position: Position
+    """Node's position inside the simulation"""
