ad424e6c0825ffb13954c86179dd99e299d286c3
thlamz <thlamenza@hotmail.com>
2023-11-07 22:05:39 -0300

    Testing showcases

--- showcases/follow-mobility/main.py
+++ showcases/follow-mobility/main.py
@@ -1,16 +1,18 @@
-from protocol import LeaderProtocol, FollowerProtocol
 from gradysim.simulator.handler.communication import CommunicationHandler
 from gradysim.simulator.handler.mobility import MobilityHandler
 from gradysim.simulator.handler.timer import TimerHandler
 from gradysim.simulator.handler.visualization import VisualizationHandler
 from gradysim.simulator.simulation import SimulationBuilder, SimulationConfiguration, PositionScheme
+from protocol import LeaderProtocol, FollowerProtocol
 
-if __name__ == '__main__':
-    builder = SimulationBuilder(SimulationConfiguration(duration=30, debug=True, real_time=True))
+
+def run_simulation(real_time: bool):
+    builder = SimulationBuilder(SimulationConfiguration(duration=30, debug=True, real_time=real_time))
     builder.add_handler(CommunicationHandler())
     builder.add_handler(TimerHandler())
     builder.add_handler(MobilityHandler())
-    builder.add_handler(VisualizationHandler())
+    if real_time:
+        builder.add_handler(VisualizationHandler())
 
     builder.add_node(LeaderProtocol, (0, 0, 0))
     for _ in range(10):
@@ -18,3 +20,7 @@ if __name__ == '__main__':
 
     simulation = builder.build()
     simulation.start_simulation()
+
+
+if __name__ == '__main__':
+    run_simulation(True)
--- showcases/ping-pong/main.py
+++ showcases/ping-pong/main.py
@@ -1,6 +1,5 @@
 from typing import List
 
-from ping import PingProtocol
 from gradysim.simulator.handler.assertion import assert_always_true_for_simulation, AssertionHandler
 from gradysim.simulator.handler.communication import CommunicationHandler
 from gradysim.simulator.handler.mobility import MobilityHandler
@@ -8,6 +7,7 @@ from gradysim.simulator.handler.timer import TimerHandler
 from gradysim.simulator.handler.visualization import VisualizationHandler
 from gradysim.simulator.node import Node
 from gradysim.simulator.simulation import SimulationBuilder, SimulationConfiguration
+from ping import PingProtocol
 
 
 @assert_always_true_for_simulation(name="received_equals_sent")
@@ -19,12 +19,14 @@ def assert_received_equals_sent(nodes: List[Node[PingProtocol]]) -> bool:
         sent += node.protocol_encapsulator.protocol.received
     return received == sent
 
+
 def run_simulation(real_time: bool):
     builder = SimulationBuilder(SimulationConfiguration(duration=30, debug=True, real_time=real_time))
     builder.add_handler(CommunicationHandler())
     builder.add_handler(TimerHandler())
     builder.add_handler(MobilityHandler())
-    builder.add_handler(VisualizationHandler())
+    if real_time:
+        builder.add_handler(VisualizationHandler())
     builder.add_handler(AssertionHandler([assert_received_equals_sent]))
 
     builder.add_node(PingProtocol, (0, 0, 0))
@@ -32,6 +34,7 @@ def run_simulation(real_time: bool):
 
     simulation = builder.build()
     simulation.start_simulation()
+
 
 if __name__ == '__main__':
     run_simulation(True)
