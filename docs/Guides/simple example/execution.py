3e43e60176165dae9b73734c8d2b7e37141d1681
Thiago Lamenza <thlamenza@hotmail.com>
2023-12-05 00:20:28 -0300

    Starting work on simple example for docs

---
+++ docs/Guides/simple example/execution.py
@@ -0,0 +1,51 @@
+from gradysim.simulator.handler.communication import CommunicationHandler
+from gradysim.simulator.handler.mobility import MobilityHandler
+from gradysim.simulator.handler.timer import TimerHandler
+from gradysim.simulator.handler.visualization import VisualizationHandler, VisualizationConfiguration
+from gradysim.simulator.simulation import SimulationBuilder, SimulationConfiguration
+from protocol import SimpleSensorProtocol, SimpleGroundStationProtocol, program_simple_uav_protocol
+
+if __name__ == '__main__':
+    config = SimulationConfiguration(
+        duration=100
+    )
+    builder = SimulationBuilder(config)
+
+    builder.add_node(SimpleSensorProtocol, (150, 0, 0))
+    builder.add_node(SimpleSensorProtocol, (0, 150, 0))
+    builder.add_node(SimpleSensorProtocol, (-150, 0, 0))
+    builder.add_node(SimpleSensorProtocol, (0, -150, 0))
+
+    builder.add_node(SimpleGroundStationProtocol, (0, 0, 0))
+
+    builder.add_node(program_simple_uav_protocol([
+        (0, 0, 20),
+        (150, 0, 20)
+    ]), (0, 0, 0))
+
+    builder.add_node(program_simple_uav_protocol([
+        (0, 0, 20),
+        (0, 150, 20)
+    ]), (0, 0, 0))
+
+    builder.add_node(program_simple_uav_protocol([
+        (0, 0, 20),
+        (-150, 0, 20)
+    ]), (0, 0, 0))
+
+    builder.add_node(program_simple_uav_protocol([
+        (0, 0, 20),
+        (0, -150, 20)
+    ]), (0, 0, 0))
+
+    builder.add_handler(TimerHandler())
+    builder.add_handler(CommunicationHandler())
+    builder.add_handler(MobilityHandler())
+    builder.add_handler(VisualizationHandler(VisualizationConfiguration(
+        x_range=(-150, 150),
+        y_range=(-150, 150),
+        z_range=(0, 150)
+    )))
+
+    simulation = builder.build()
+    simulation.start_simulation()
