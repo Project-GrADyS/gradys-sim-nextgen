f86cfef2c8f11809f18668754ce7307693150e73
Thiago Lamenza <thlamenza@hotmail.com>
2025-08-23 13:37:25 -0300

    Exposing main types in __init__ for easier importing

---
+++ gradysim/protocol/plugin/raft/__init__.py
@@ -0,0 +1,10 @@
+from .raft_config import RaftConfig, FailureConfig, RaftMode
+from .raft_consensus import RaftConsensus
+
+
+__all__ = [
+    "RaftConfig",
+    "FailureConfig",
+    "RaftMode",
+    "RaftConsensus"
+]
